0e44825eba6ed14ad5d1d58904068792b421d72e
Sofia94 <sofiarchi94@gmail.com>
2021-09-22 10:38:58 -0500

    Validadcion funcion correo

---
+++ form.js
@@ -0,0 +1,21 @@
+function checkCorreo(){
+    let email =document.getElementById("correo").value;
+    let regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if (email==null || email.length==0){
+        alert("Campo correo es obligatorio");
+        return false;
+    } 
+
+    else if (regex.test(email)== false){
+        alert("ingrese un correo valido");
+        return false;    
+    }
+
+    else{
+        return true;
+    }
+}
+
+function validate(){
+    checkCorreo();
+}
