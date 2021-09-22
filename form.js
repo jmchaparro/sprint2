199788bce83a5977ae95a10c04c31aa3bd622081
jmchaparro <juanmanuel@jmchaparro.com>
2021-09-22 10:39:14 -0500

    prueba de git

--- form.js
+++ form.js
@@ -1,21 +1,19 @@
-function checkCorreo(){
-    let email =document.getElementById("correo").value;
-    let regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-    if (email==null || email.length==0){
-        alert("Campo correo es obligatorio");
-        return false;
-    } 
-
-    else if (regex.test(email)== false){
-        alert("ingrese un correo valido");
-        return false;    
-    }
-
-    else{
-        return true;
-    }
+function checkCorreo() {
+  let email = document.getElementById("correo").value;
+  let regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  if (email == null || email.length == 0) {
+    alert("Campo correo es obligatorio");
+    return false;
+  } else if (regex.test(email) == false) {
+    alert("ingrese un correo valido");
+    return false;
+  } else {
+    return true;
+  }
 }
 
-function validate(){
-    checkCorreo();
+function validate() {
+  checkCorreo();
 }
+
+// esto es una prueba de git
