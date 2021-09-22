4277d7020bc032e964e2395a16699153b1500fa8
DeLaHoz73 <90298773+DeLaHoz73@users.noreply.github.com>
2021-09-22 14:22:33 -0500

    Update form.js

--- form.js
+++ form.js
@@ -12,6 +12,23 @@ function checkCorreo() {
   }
 }
 
+function checkNombre() {
+  var validanombre = document.getElementById('nombre');
+  if (validanombre.value.length == 0) {
+      validanombre.style.border= '1px solid #f50808'; 
+      alert("false 1");
+      return false;
+  } else if (!/^[a-zA-ZÀ-ÿ\s]{4,30}$/.test(validanombre.value)) {
+      validanombre.style.border= '1px solid #f50808'; 
+      alert("false 2");
+      return false;
+  } else {
+      validanombre.style.border= '1px solid #dee2e6'; 
+      alert("true");
+      return true;
+  }
+}
+
 function validate() {
   checkCorreo();
 }
